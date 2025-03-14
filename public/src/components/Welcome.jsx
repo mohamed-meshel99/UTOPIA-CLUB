acae325b6e22f1d756b415d27aa0ec01993bcf34
mohamed-meshel99 <154675010+mohamed-meshel99@users.noreply.github.com>
2025-03-14 21:17:17 +0200

    Add files via upload

---
+++ public/src/components/Welcome.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Robot from "../assets/robot.gif";
+export default function Welcome() {
+  const [userName, setUserName] = useState("");
+  useEffect(async () => {
+    setUserName(
+      await JSON.parse(
+        localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+      ).username
+    );
+  }, []);
+  return (
+    <Container>
+      <img src={Robot} alt="" style={{ width: '100px', height: '100px' }} />
+
+      <h1>
+        Hello, <span>{userName}</span>
+      </h1> <br />
+      <h3>tap on any picture and make friends</h3>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  flex-direction: column;
+  img {
+    height: 20rem;
+  }
+  span {
+    color: rgb(61, 33, 138);
+  }
+`;
